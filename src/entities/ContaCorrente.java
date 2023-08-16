35aeb9d9b923ffaa88147689b5234de47ee80974
WillOli <william.ika@gmail.com>
2023-08-15 23:24:15 -0300

    Implementando os metodos depositar e sacar conta corrente.

--- src/entities/ContaCorrente.java
+++ src/entities/ContaCorrente.java
@@ -1,6 +1,10 @@
 package entities;
 
+import java.util.Scanner;
+
 public class ContaCorrente {
+
+    Scanner input = new Scanner(System.in);
 
     /* total de saques realizados na conta */
     private double SaldoAtualContaCorrente = 0;
@@ -18,6 +22,39 @@ public class ContaCorrente {
     /* total de depositos realizados na conta */
     private double totalDepositosContaCorrente = 0;
     private double totalDepositosGeral = 0;
+
+    /*Metodos depositar conta corrente*/
+    public void depositarContaCorrente() {
+        System.out.print("\t Quanto depositar na conta corrente: R$ ");
+        double depositarContaCorrente = input.nextDouble();
+        System.out.println("\t ...Depositando R$ " + depositarContaCorrente + " na Conta Corrente...");
+        this.SaldoAtualContaCorrente += depositarContaCorrente;
+        this.totalDepositosGeral++;
+        this.totalDepositosContaCorrente++;
+        this.totalDepositosContaCorrente += depositarContaCorrente;
+        this.totalDepositadoGeral += depositarContaCorrente;
+        System.out.println("\t Saldo atual conta corrente: R$ " + this.getSaldoAtualContaCorrente());
+    }
+
+    /* metodo scar conta corrente*/    public void sacarContaCorrente() {
+        System.out.println("\t Saldo atual conta corrente: R$ " + this.getSaldoAtualContaCorrente());
+        System.out.println("\t Quanto sacar da conta corrente: R$");
+
+        double retiraContaCorrente = input.nextDouble();
+        if (retiraContaCorrente <= this.getSaldoAtualContaCorrente()) {
+            System.out.println("\n\t ...Sacando R$ " + retiraContaCorrente + " da Conta Corrente...");
+            this.SaldoAtualContaCorrente -= retiraContaCorrente;
+            this.totalSaquesGeral++;
+            this.totalSaquesContaCorrente++;
+            this.totalSacadoContaCorrente += retiraContaCorrente;
+            this.totalSacadoGeral += retiraContaCorrente;
+            System.out.println("\t Saldo atual conta corrente: R$ " + this.getSaldoAtualContaCorrente());
+        }
+        else {
+            System.out.println("\t Você não possui saldo suficiente para sacar essa quantidade no momento!");
+            System.out.println("\t Seu saldo atual é: " + this.getSaldoAtualContaCorrente());
+        }
+    }
 
 
     public double getSaldoAtualContaCorrente() {
